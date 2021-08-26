624dd1acd2aae957f8fee55cdcf372b7beda11b1
RONDEAU Felix <79422543+xil-f-dev@users.noreply.github.com>
2021-08-26 13:21:01 +0200

    adding BAN command

---
+++ commands/Moderation/ban.js
@@ -0,0 +1,27 @@
+const { errorMessage, successMessage } = require("../../utils/infoMessages");
+
+module.exports.run = async (client, message, args) => {
+	let member = message.mentions.members.first();
+
+	if (!member.bannable)
+		return errorMessage(`Je ne peux pas ban ${message.mentions.users.first()} !`, message.channel);
+
+	const reason = args.length > 1 ? args.splice(1).join(" ") : "";
+
+	member
+		.ban({ reason: reason })
+		.then(successMessage(`${message.mentions.users.first()} à bien été ban du serveur !`, message.channel));
+};
+
+module.exports.help = {
+	name: "ban",
+	aliases: [],
+	description: "Ban the member",
+	usage: "<command> @member [reason]", // '[]' for not necessary args and '||' for OR symbol
+	cooldown: "10", // sec
+	cooldownType: "command", // 'user' || 'command'
+	authNeeded: "BAN_MEMBERS", // eg. KICK_MEMBERS
+	delete: true,
+	mention: true,
+	args: false,
+};
